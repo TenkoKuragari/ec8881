1637a20439deb4f08a246d16dd024aeac8e6639d
Ethan Chen <ec8881@rit.edu>
2025-02-10 12:29:17 -0500

    w

---
+++ lectures/week5_src_starter/Pokemon.java
@@ -0,0 +1,37 @@
+package jcf;
+
+public class Pokemon {
+
+    public enum Type {GROUND, WATER, FIRE, FLYING}
+
+    private final String name;
+    private final int pokedex;
+    private final Type type;
+
+    public Pokemon(String name, int pokedex, Type type) {
+        this.name = name;
+        this.pokedex = pokedex;
+        this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPokedex() {
+        return pokedex;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "name='" + name + '\'' +
+                ", pokedex=" + pokedex +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ lectures/week5_src_starter/PokemonStadium.java
@@ -0,0 +1,63 @@
+package jcf;
+
+import java.util.*;
+
+public class PokemonStadium {
+    private Pokemon charmander;
+    private Pokemon squirtle;
+    private Pokemon starly;
+    private Pokemon diglett;
+    private Pokemon hoothoot;
+    private Pokemon magikarp;
+    private List<Pokemon> pokemonList;
+
+    public PokemonStadium() {
+        pokemonList = new ArrayList<>();
+        this.charmander = new Pokemon("Charmander", 4, Pokemon.Type.FIRE);
+        this.starly = new Pokemon("Starly", 396, Pokemon.Type.FLYING);
+        this.hoothoot = new Pokemon("Hoothoot", 163, Pokemon.Type.FLYING);
+        this.diglett = new Pokemon("Digglet", 150, Pokemon.Type.GROUND);
+        this.magikarp = new Pokemon("Magikarp", 129, Pokemon.Type.WATER);
+        this.squirtle = new Pokemon("Squirtle", 7, Pokemon.Type.WATER);
+
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        PokemonStadium stadium = new PokemonStadium();
+        //TODO
+    }
+
+    private void findPokemon() {
+        System.out.println("Finding a pokemon .....");
+        //TODO
+    }
+
+    private void orderedStrings() {
+        Set<String> strings = new TreeSet<>();
+        strings.add("c");
+        strings.add("a");
+        strings.add("l");
+        strings.add("b");
+        strings.add("d");
+
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+
+    private void orderPokemons() {
+        System.out.println("Printing the pokemons in order");
+        // TODO
+    }
+
+    private void orderSpecialPokemons() {
+        System.out.println("Printing the pokemons in a special order");
+        // TODO
+    }
+
+    private void findPokemonFast() {
+        System.out.println("Finding pokemon in O(1) time");
+        // TODO
+    }
+}
